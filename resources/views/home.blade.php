f6284cc3049c86ee96e8095c6c3994e2bcb309cf
Prakash Carpenter <prakashprince001@gmail.com>
2023-02-17 02:21:46 +0530

    Add Model Migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">
+                    {{ __('Dashboard') }}
+                    <button type="button" class="btn btn-primary pull-right" style="margin-right: 10px;">Add Product</button>
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -14,7 +16,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <!-- {{ __('You are logged in!') }} -->
                     <table id="product" class="display table table-bordered">
                         <thead>
                             <tr>
